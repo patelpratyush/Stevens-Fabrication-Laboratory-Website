47cda42700fa7153613ffbe62b76730698e38fa7
Pratyush Patel <patelpratyush28@gmail.com>
2025-11-20 17:51:38 -0500

    Add basic layout and starter pages

--- client/src/app/layout.js
+++ client/src/app/layout.js
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import "./globals.css";
 
 export const metadata = {
@@ -13,21 +14,21 @@ export default function RootLayout({ children }) {
           <nav className="max-w-5xl mx-auto flex items-center justify-between p-4">
             <span className="font-semibold text-lg">Stevens Fab Lab</span>
             <div className="flex gap-4 text-sm">
-              <a href="/" className="hover:underline">
+              <Link href="/" className="hover:underline">
                 Home
-              </a>
-              <a href="/fabrication" className="hover:underline">
+              </Link>
+              <Link href="/fabrication" className="hover:underline">
                 Fabrication
-              </a>
-              <a href="/equipment" className="hover:underline">
+              </Link>
+              <Link href="/equipment" className="hover:underline">
                 Equipment
-              </a>
-              <a href="/order" className="hover:underline">
+              </Link>
+              <Link href="/order" className="hover:underline">
                 Order
-              </a>
-              <a href="/dashboard" className="hover:underline">
+              </Link>
+              <Link href="/dashboard" className="hover:underline">
                 Dashboard
-              </a>
+              </Link>
             </div>
           </nav>
         </header>
